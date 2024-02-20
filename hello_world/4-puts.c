63a8c78fdbd23cf3550b34ddf1b648c72c66fdfe
AngelHCS <8990holbertonstudents.com>
2024-02-19 20:16:03 -0400

    Closed comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) /; grabs void which does not return file"
+int main(void) /; grabs void which does not return fil;/e"
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
