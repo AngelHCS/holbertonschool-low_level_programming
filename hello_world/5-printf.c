e34bc1fd643a988cae36cdbf8f88d0e5dfd7346a
AngelHCS <8990holbertonstudents.com>
2024-02-19 21:00:05 -0400

    C 4

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) /*keep getting a syntad error on this line, near ( */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
