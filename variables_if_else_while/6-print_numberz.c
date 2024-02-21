07dfffd9671a2b45d98b20c7561431e7870bb59c
AngelHCS <8990holbertonstudents.com>
2024-02-20 23:20:47 -0400

    print single digits with putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers with putchar
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+putchar(digit + '0');
+
+putchar ('\n');
+
+return (0);
+}
