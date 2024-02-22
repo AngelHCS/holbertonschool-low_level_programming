d662850a39d7608b08b557012dc6f96c7530c24e
AngelHCS <8990holbertonstudents.com>
2024-02-21 20:05:39 -0400

    0 throws a multi char warning with gcc command

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * main- void
+ *
+ *
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('/n'); /* new line multi char warn, im pushing anyways*/
+
+
+
+	return (0);
+}
