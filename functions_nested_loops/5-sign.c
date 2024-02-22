1744839284282caecb72ac4ed333b9dd6027bb1c
AngelHCS <8990holbertonstudents.com>
2024-02-21 23:33:28 -0400

    removed one of the betty format warnings

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n tge int to check
+ * @n: the int to check
  * Return 1 and prints + if n is greater than zero
- *and prints  0 if n is zero
+ * and prints  0 if n is zero
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
