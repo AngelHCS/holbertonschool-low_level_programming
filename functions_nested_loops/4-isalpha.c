834b14634df7e074cb8af1d1f5e2178817e24a26
AngelHCS <8990holbertonstudents.com>
2024-02-22 09:21:10 -0400

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha - check for alphabetical char
- * @c: the char to be checked
+ * _isalpha - check for alphabetical character
+ * @c: the character to be checked
  * Return : 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
