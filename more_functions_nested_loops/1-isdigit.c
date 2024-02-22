8765559848045970471fdc4ea1c6a2666419c25e
AngelHCS <8990holbertonstudents.com>
2024-02-22 13:37:46 -0400

    1 find if c is digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_isdigit - checks for a digit
+ *@c: check if it is a digit
+ *
+ *Return: has to be 1 if parameter is a digit 0 if not
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	else
+{
+	return (0);
+}
+
+}
+
