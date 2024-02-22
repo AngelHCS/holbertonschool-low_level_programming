b2fb6b9cde13674a26216d189fe5adfd3ea1c9fe
AngelHCS <8990holbertonstudents.com>
2024-02-22 10:37:52 -0400

    starting the project up

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main- prints _isupper
+*
+* Return: 1 if c is uppercase 0 otherwise
+*/
+
+void _isupper int (c)
+
+{
+
+	char c;
+
+	if c >= 'A' && c >= 'Z'
