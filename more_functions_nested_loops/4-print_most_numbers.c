faca9377109820aba327ccab4abd9199051f9eb4
AngelHCS <8990holbertonstudents.com>
2024-02-23 10:55:10 -0400

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 12; c < 22; c++)
+	for (c = 48; c < 58; c++)
 	{
-		if (c != 14)
+		if (c != 50)
 		{
-			if (c != 16)
+			if (c != 52)
 			{
 				_putchar(c);
 			}
