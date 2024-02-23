1ac18858bba25f63bf8ac24d1f4926b6e1c8a249
AngelHCS <8990holbertonstudents.com>
2024-02-22 21:46:36 -0400

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line _ draws a straight line in terminal
+ * @n: number of times the char should print
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int a;
+
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
