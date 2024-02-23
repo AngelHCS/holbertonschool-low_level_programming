480fd6d0864d6748464f9566adc5df824e588df6
AngelHCS <8990holbertonstudents.com>
2024-02-22 20:16:34 -0400

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers
+ *
+ * Return: 0 if successful
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	_putchar (n);
+	}
+	_putchar ('\n');
+
+return (0);
+}
