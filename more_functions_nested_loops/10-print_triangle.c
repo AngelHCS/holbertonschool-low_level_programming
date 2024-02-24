b2ab8ef5ea2d05641adc93f80534ef37ce386894
AngelHCS <8990holbertonstudents.com>
2024-02-23 21:47:42 -0400

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,33 @@
 #include "main.h"
+/**
+ *print_triangle - prints a triangle with newline
+ *@size: size of triangle
+ *Return: always 0
+ */
+void print_triangle(int size)
+
+{
+	if (size <= 0)
+	{
+	_putchar('n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j < size; j++)
+			{
+				_putchar (' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#')
+			}
+
+			_putchar('\n')
+		}
+	}
+}
