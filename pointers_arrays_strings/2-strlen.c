cb53b7c6b608e4d464a4298ea73ca0ed8140ac5f
AngelHCS <8990holbertonstudents.com>
2024-02-26 14:42:08 -0400

    2nd report

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,18 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strlen - return length of a string
+ *@s:value for string
+ *Return: string 1-8
  */
- int _strlen(char *s)
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
