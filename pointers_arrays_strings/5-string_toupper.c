804bec4fa712ab6f32db915ccfa845ec9780d1ea
AngelHCS <8990holbertonstudents.com>
2024-02-28 23:11:29 -0400

    did 5 and 6, had to revisit 5 after i got an error and moved to 6

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *string_toupper - change lowercase to uppercase
+ *@s: the string to change to uppercase
+ *Return: string in uppercase
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) = *(s + i) - 32;
+		}
+		i++;
+	}
+	return (s);
+}
