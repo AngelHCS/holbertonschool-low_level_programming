04f752089e9ff7613a9f3369546618b8fd50e348
AngelHCS <8990holbertonstudents.com>
2024-02-29 11:41:32 -0400

    0 fill with constant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_memset - fill memory with constant byte
+ *@s: pointer to memory area
+ *@n: bytes of memory
+ *@b: the constant byte
+ *Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k;
+
+		for (k = 0; k < n; k++)
+		{
+			s[k] = b;
+		n -= 1;
+		}
+	return (s);
+}
