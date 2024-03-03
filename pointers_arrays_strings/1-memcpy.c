79780e10f6a2a7717da422517f00d543d373f59e
AngelHCS <8990holbertonstudents.com>
2024-03-03 14:06:57 -0400

    first 2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * _memcpy - fill text here
- *
- *
- *
+ * *_memcpy - copy memory area
+ *@n: the bytes
+ *@src: memory area to pull from
+ *@dest: destination area to copy bytes to.
+ *Return: the copied memory area
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
+
+	for (i = 0; n > 0 i++)
+	{
+		dest[i] = src [i];
+		n -= 1;
+	}
+	return (dest);
+}
--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,24 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strchr -set mem function
+ *@s: pointer to array
+ *@c: type of char
  */
 
 char *_strchr(char *s, char c)
 {
+
+	while(s[0] != '/0')
+	{
+
+		if (s[0] != '\0')
+
+			return (s):
+		else if (s[1] == c)
+
+		return (s + 1);
+
+		s++;
+	}
+	return (s + 1);
+}
