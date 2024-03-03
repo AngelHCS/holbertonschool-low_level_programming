a24d9945ab20ce5e7300dfe7b6c262c41733e694
AngelHCS <8990holbertonstudents.com>
2024-03-03 15:03:44 -0400

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,9 +1,21 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strstr - locates substring
+ *@needle: pointer 1 char
+ *@haystack: pointer 2 char
+ * Return: s (substring?)
  */
 
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
 {
+	int count;
+
+	for (; haystack[0]; haystack++)
+	{
+		for (count = 0; haystack[count] == needle[count]; count++)
+			;
+		if (!(needle[count]))
+			return (haystack);
+	}
+	return (0);
+}
