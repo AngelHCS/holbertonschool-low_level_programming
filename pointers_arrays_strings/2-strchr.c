0022b0ce8ccd4f0a41b9c9d6ab75599808e3d1e1
AngelHCS <8990holbertonstudents.com>
2024-03-03 15:12:36 -0400

    large commit of files

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,17 +3,18 @@
  * _strchr -set mem function
  *@s: pointer to array
  *@c: type of char
+ *Return: string s
  */
 
 char *_strchr(char *s, char c)
 {
 
-	while(s[0] != '/0')
+	while (s[0] != '\0')
 	{
 
-		if (s[0] != '\0')
+		if (s[0] == c)
 
-			return (s):
+			return (s);
 		else if (s[1] == c)
 
 		return (s + 1);
