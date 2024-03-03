feb91e531c8f881ff99915fc6a9b9514778e57eb
AngelHCS <8990holbertonstudents.com>
2024-03-03 14:55:17 -0400

    4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,23 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strpbrk - search a string for a set of bytes
+ * @s: char pointer
+ *@accept: char pointer2
+ * Return: string
  */
 
-char *_strpbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept)
 {
+	unsigned int counter;
+
+	for (; *s != '\0'; s++)
+	{
+		for (counter = 0; accept[counter] != '\0'; counter++)
+		{
+
+			if (*s == accept[counter])
+				return (s);
+		}
+	}
+	return (0);
+}
