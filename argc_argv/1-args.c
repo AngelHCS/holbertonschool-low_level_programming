73c63425f41c0d7715b878e1fe019a5679a2c7c2
AngelHCS <8990holbertonstudents.com>
2024-03-05 23:52:15 -0400

    gives the number of arguments on bash

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * argc: argument count to be passed
+ * argv: Pointer that tells where the arguments will go
+ * Return: 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
