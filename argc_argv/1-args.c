fb961c26b757204318dffa511a70b0e3ddfc0c76
AngelHCS <8990holbertonstudents.com>
2024-03-05 23:53:05 -0400

    fixed betty format

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 /**
  * main - entry point
- * argc: argument count to be passed
- * argv: Pointer that tells where the arguments will go
- * Return: 0 success
+ *@argc: argument count to be passed
+ *@argv: Pointer that tells where the arguments will go
+ *Return: 0 success
  */
 
 int main(int argc, char **argv)
