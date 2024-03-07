66a460da878391c1ad03508555fed3d569cfb5f2
AngelHCS <8990holbertonstudents.com>
2024-03-06 21:04:36 -0400

    fixed betty format

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -35,20 +35,20 @@ int main(int argc, char **argv)
 {
 	int i, n, ex;
 
-	ex = 0, n= 0;
+	ex = 0, n = 0;
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if(_isnumber(argv[i]))
+			if (_isnumber(argv[i]))
 				n += atoi(argv[i]);
 			else
 				ex = 1;
 		}
 	}
 	if (ex == 0)
-		printf ("%i\n" , n);
+		printf("%i\n", n);
 	else
-		printf ("%s\n", "Error");
+		printf("%s\n", "Error");
 	return (ex);
 }
