ebdad60d70d86a45ab04abffffed577f909716de
AngelHCS <8990holbertonstudents.com>
2024-03-07 13:13:52 -0400

    fixed betty format

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 #include "main.h"
 /**
  *main- print name
- *argc: count arguments
- *argv: argument vector (i thought it was arg value this whole time)
+ *@argc: count arguments
+ *@argv: argument vector (i thought it was arg value this whole time)
  *Return: 0
  */
 
-int main(int argc, char ** argv);
+int main(int argc, char **argv);
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
 
-	return(0);
+	return (0);
 }
