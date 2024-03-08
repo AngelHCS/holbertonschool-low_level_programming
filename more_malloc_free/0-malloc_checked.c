864c583fd8c1bd8688b5899fddcf0a0e6f551b4c
AngelHCS <8990holbertonstudents.com>
2024-03-08 19:57:23 -0400

    start

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * *malloc_checked - allocate memory
+ * @b: unsigned int
+ * Return: return
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit (98);
+	return (ptr);
+}
