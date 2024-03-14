894c527fc1e7c756d6cffd129604ddd6795ca364
AngelHCS <8990holbertonstudents.com>
2024-03-14 12:22:28 -0400

    all

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *op_add - operation_addition
+ *@a: first num
+ *@b: second num
+ *Return: addition result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - operation_subtraction
+ * @a:first num
+ * @b:second num
+ * Return: subtraction result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - operation_multiplication
+ * @a:first num
+ * @b:second num
+ * Return: multiplication result
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+			exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - operation_modulo
+ * @a: first num
+ * @b: second num
+ * Return: modulo result
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+			exit(100);
+	}
+	return (a % b);
+}
