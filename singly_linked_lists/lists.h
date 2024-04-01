0a24edb7671b61be2367cadb20af9fc5c8da954d
AngelHCS <8990holbertonstudents.com>
2024-04-01 07:29:46 -0400

    update for list.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+
+size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
