b94da464a6e47ec8a5465a886fda4702c21d9ca7
AngelHCS <8990holbertonstudents.com>
2024-04-01 23:20:34 -0400

    4-free file

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,1 +1,20 @@
 #include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the first node of list.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
